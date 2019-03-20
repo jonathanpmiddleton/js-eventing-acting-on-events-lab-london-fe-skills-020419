e3527b9e1ef26c85700abedda22a57c6aa60c587
Jm <jonathan.p.middleton@gmail.com>
2019-03-20 19:24:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var dodger = document.getElementById('dodger');
 function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10);
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`;
+  if (left >= 360) {
+    dodger.style.left = `${left + 0}px`;
+  }
+  
+  else if (left > 0) {
+    dodger.style.left = `${left + 10}px`;
   }
 }
 
